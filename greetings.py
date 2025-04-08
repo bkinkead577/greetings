c7ab5b8853b6de091f0efda68eca603ce3f510b1
Brian Kinkead <bkinkead7@gmail.com>
2025-04-08 10:46:17 -0400

    Add translator.

--- greetings.py
+++ greetings.py
@@ -1,7 +1,10 @@
 from greets import greetings
+from translate import Translator
+
+translator = Translator(to_lang='pt')
 
 if __name__ == "__main__":
     for g in greetings:
-        print(g.title())
+        print(translator.translate(g).title())
 
 
